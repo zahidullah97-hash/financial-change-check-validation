4f9853882d9719af5a8f8325671ae058fe3c4a77
zahidullah97-hash <zahidullah97@gmail.com>
2026-08-31 12:12:37 +0200

    Add Prolific completion redirect

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.STUDY_CONFIG = {
   SUPABASE_URL: "https://vhnyrqczgbtkwgvxelty.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_8UpEn-fzDfH0OB7SKnSbfw_eGtUFRqE",
-  PROLIFIC_COMPLETION_URL: ""
+ PROLIFIC_COMPLETION_URL: "https://app.prolific.com/submissions/complete?cc=C1OWG3U9"
 };
